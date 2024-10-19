1100a1eacd6b577d740a21a479df0293f7e56cec
Aaryan <aaryan24602@gmail.com>
2024-10-19 14:32:19 +0530

    updated unused vars

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -19,7 +19,7 @@ if (!cached) {
 export const connectToDatabase = async () => {
   if (cached.conn) return cached.conn;
 
-  if (!MONGODB_URL) throw new Error("Missing mongodb URL");
+  if (!MONGODB_URL) throw new Error("Missing MONGODB_URL");
 
   cached.promise =
     cached.promise ||
@@ -29,5 +29,6 @@ export const connectToDatabase = async () => {
     });
 
   cached.conn = await cached.promise;
+
   return cached.conn;
 };
